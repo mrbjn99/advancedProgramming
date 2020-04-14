6e6787ef3d63bd6b5a56686a6345c91bbdea58d1
mrbjn99 <58242504+mrbjn99@users.noreply.github.com>
2020-04-14 10:22:00 +0700

    code mau abstraction

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Abstraction
+{
+    // abstract class 
+    abstract class Shape
+    {
+
+        // abstract method 
+        public abstract int area();
+    }
+
+    // square class inherting 
+    // the Shape class 
+    class Square : Shape
+    {
+
+        // private data member 
+        private int side;
+
+        // method of  square class 
+        public Square(int x = 0)
+        {
+            side = x;
+        }
+
+        // overriding of the abstract method of Shape 
+        // class using the override keyword 
+        public override int area()
+        {
+            Console.Write("Area of Square: ");
+            return (side * side);
+        }
+    }
+
+    // Driver Class 
+    class GFG
+    {
+
+        // Main Method 
+        static void Main(string[] args)
+        {
+
+            // creating reference of Shape class 
+            // which refer to Square class instance 
+            Shape sh = new Square(4);
+
+            // calling the method 
+            double result = sh.area();
+
+            Console.Write("{0}", result);
+
+        }
+    }
+}
